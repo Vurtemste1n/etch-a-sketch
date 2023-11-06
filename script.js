b5c7ba9d5ba7cf3227e6516a5242800099ab7caf
Vurtemste1n <danil@armanshin.ru>
2023-11-06 19:23:43 +0300

    Added button for changing grid size

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
-const container = document.querySelector('.container');
-    for (let i = 1; i<=16; i++) {
-    const column = document.createElement('div');
+function toBuiltGrid(gridSize) {
+  for (let i = 1; i <= gridSize; i++) {
+    const column = document.createElement("div");
     container.appendChild(column);
     column.classList.add("column");
-    for (let j = 1; j <= 16; j++) {
-        const row = document.createElement('div');
-        column.appendChild(row);
-        row.classList.add("row");
-        row.addEventListener('click', () => {
-            row.classList.add('color');
-        })
-        }
+    for (let j = 1; j <= gridSize; j++) {
+      const row = document.createElement("div");
+      column.appendChild(row);
+      row.classList.add("row");
+      row.addEventListener("mouseover", () => {
+        row.classList.add("color");
+      });
     }
+  }
+}
+function gridSizePromt() {
+  let size;
+  let userChoice = prompt("Enter desirable grid size: ");
+  if (Number(userChoice) <= 100) {
+    size = Number(userChoice);
+  } else alert("ERROR:your number is higher than 100");
+  return size;
+}
+const container = document.querySelector(".container");
+const gridSizeButton = document.querySelector(".grid-size");
+let gridSize;
+gridSizeButton.addEventListener("click", () => {
+  gridSize = gridSizePromt();
+  toBuiltGrid(gridSize);
+});
+container.addEventListener("click", () => {
+  toBuiltGrid(16);
+});
